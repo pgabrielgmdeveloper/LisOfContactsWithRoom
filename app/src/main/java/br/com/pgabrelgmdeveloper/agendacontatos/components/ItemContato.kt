192053c5643454c6d8a22eb887556f6ea419a04c
pgabrielgmdeveloper <161240639+pgabrielgmdeveloper@users.noreply.github.com>
2024-03-24 15:52:16 -0300

    adicionado funçãos de deleção e navegação para tela de atualização de contato

--- app/src/main/java/br/com/pgabrelgmdeveloper/agendacontatos/components/ItemContato.kt
+++ app/src/main/java/br/com/pgabrelgmdeveloper/agendacontatos/components/ItemContato.kt
@@ -1,8 +1,12 @@
 package br.com.pgabrelgmdeveloper.agendacontatos.components
 
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -10,6 +14,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,46 +24,57 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
 import br.com.pgabrelgmdeveloper.agendacontatos.R
+import br.com.pgabrelgmdeveloper.agendacontatos.model.Contato
 import br.com.pgabrelgmdeveloper.agendacontatos.ui.theme.ShapesCardsView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 @Composable
-fun ItemContato(){
-
+fun ItemContato(
+    navigateToAtt: () -> Unit,
+    deleteItem: () -> Unit,
+    context: Context,
+    contato: Contato
+){
+    val scope = rememberCoroutineScope()
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().padding(10.dp, 5.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
         shape = ShapesCardsView.small,
-        colors =CardDefaults.cardColors(containerColor = Color.White)
+        colors =CardDefaults.cardColors(containerColor = Color.White),
+
     ) {
-        ConstraintLayout(modifier = Modifier
-            .align(Alignment.CenterHorizontally)) {
+        ConstraintLayout() {
             val (txtContato, txtIdade, txtNumero, iconEdit, iconDelete) = createRefs()
             Text(
-                text = "Contato: PAULO MATIAS",
+                text = "Contato: ${contato.name} ${contato.sobrename} ",
                 modifier = Modifier.constrainAs(txtContato) {
                     top.linkTo(parent.top, margin = 30.dp)
-                    start.linkTo(parent.start, margin = 0.dp)
+                    start.linkTo(parent.start, margin = 10.dp)
                 }
             )
             Text(
-                text = "Idade: 24",
+                text = "Idade: ${contato.idade}",
                 modifier = Modifier.constrainAs(txtIdade) {
                     top.linkTo(txtContato.bottom, margin = 5.dp)
-                    start.linkTo(parent.start, margin = 0.dp)
+                    start.linkTo(parent.start, margin = 10.dp)
                 }
             )
             Text(
-                text = "Numero: 79988498785",
+                text = "Numero: ${contato.idade}",
                 modifier = Modifier.constrainAs(txtNumero) {
                     top.linkTo(txtIdade.bottom, margin = 5.dp)
-                    start.linkTo(parent.start, margin = 0.dp)
+                    start.linkTo(parent.start, margin = 10.dp)
                     bottom.linkTo(parent.bottom, margin = 30.dp)
                 }
             )
             Button(
-                onClick = { },
+                onClick = {
+                  navigateToAtt()
+                },
                 colors = ButtonDefaults.buttonColors(containerColor = Color.White),
                 elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp, disabledElevation = 0.dp),
                 modifier = Modifier.constrainAs(iconEdit){
@@ -69,7 +85,25 @@ fun ItemContato(){
                 Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_edit), contentDescription = "Icone de editar" )
             }
             Button(
-                onClick = { },
+                onClick = {
+                    val dialog = AlertDialog
+                        .Builder(context)
+                    dialog.setTitle("Tem certesa ?")
+                        .setMessage("Ao clicar em ok o contato sera excluido permanentemente")
+
+                    dialog.setPositiveButton("Sim") {_,_ ->
+
+                        deleteItem()
+
+                        scope.launch(Dispatchers.Main) {
+                            Toast.makeText(context,"Contato Deletado", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    dialog.setNegativeButton("Não") {_,_ ->
+
+                    }
+                    dialog.show()
+                },
                 colors = ButtonDefaults.buttonColors(containerColor = Color.White),
                 elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp, disabledElevation = 0.dp),
                 modifier = Modifier.constrainAs(iconDelete){
@@ -84,8 +118,3 @@ fun ItemContato(){
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewItemContato(){
-    ItemContato()
-}
